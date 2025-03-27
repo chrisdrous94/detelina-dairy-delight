47f430a584eca3b99d9e867241b733bf798b82b8
chrisdrous94 <christoforosdrousiotis@gmail.com>
2025-03-27 17:03:36 +0200

    new

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
-  base: '/detelina-dairy-delight/', // 👈 Add this line!
+  base: '/detelina-dairy-delight/',
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
